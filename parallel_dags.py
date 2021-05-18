3da04f11fafd96c2e0d381102c28055dc0caf545
fraser-mac <55192202+fraser-mac@users.noreply.github.com>
2021-05-18 17:13:31 +0100

    Using TaskGroups to group similar tasks together for processing

--- parallel_dags.py
+++ parallel_dags.py
@@ -1,33 +1,38 @@
 from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from airflow.utils.task_group import TaskGroup
 
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 from datetime import datetime
 
-from airflow.operators.bash import BashOperator
+
 
 default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-with DAG('parallel_dag', schedule_interval='@daily', default_args=default_args, max_active_runs=1, catchup=True) as dag:
+with DAG('parallel_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
     first_task = BashOperator(
         task_id='first_task',
         bash_command='sleep 3'
     )
 
-    second_task = BashOperator(
-        task_id='second_task',
-        bash_command='sleep 3'
-    )
+    with TaskGroup('processing_tasks') as processing_tasks:
+        second_task = BashOperator(
+            task_id='second_task',
+            bash_command='sleep 3'
+        )
 
-    third_task = BashOperator(
-        task_id='third_task',
-        bash_command='sleep 3'
-    )
+        third_task = BashOperator(
+            task_id='third_task',
+            bash_command='sleep 3'
+        )
 
     fourth_task = BashOperator(
         task_id='fourth_task',
         bash_command='sleep 3'
     )    
 
-    first_task >> [second_task, third_task] >> fourth_task 
+    first_task >> processing_tasks >> fourth_task 
     
