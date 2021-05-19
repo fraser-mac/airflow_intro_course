4e33763b95d43509a3a6f6f4a18832b7cf508294
fraser-mac <55192202+fraser-mac@users.noreply.github.com>
2021-05-19 11:47:18 +0100

    Changes to use the conditional operator
    Usage of the BranchPythonOperator operator to enable conditional checking in Airflow

--- xcom_dag.py
+++ xcom_dag.py
@@ -1,8 +1,9 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.subdag import SubDagOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.operators.dummy import DummyOperator
 
 from random import uniform
 from datetime import datetime
@@ -23,7 +24,11 @@ def _choose_best_model(ti):
         'processing_tasks.training_model_b', 
         'processing_tasks.training_model_c'
     ])
-    print (accuracies)
+
+    for accuracy in accuracies:
+        if accuracy > 5:
+            return 'accurate'
+    return 'inaccurate'
 
 with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
@@ -49,9 +54,18 @@ with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catc
             python_callable=_training_model
         )
 
-    choose_model = PythonOperator(
+    choose_model = BranchPythonOperator(
         task_id='choose_model',
         python_callable=_choose_best_model
     )
 
+    accurate = DummyOperator(
+        task_id='accurate'
+    )
+
+    inaccurate = DummyOperator(
+        task_id='inaccurate'
+    )
+
     downloading_data >> processing_tasks >> choose_model
+    choose_model >> [accurate, inaccurate]
